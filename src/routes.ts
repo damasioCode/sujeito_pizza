7de7330fa902a139e5ef30b92960770bf22e6b1e
Daniel Damasio <damasio.dan7@gmail.com>
2023-01-16 22:24:10 -0300

    first commit

---
+++ src/routes.ts
@@ -0,0 +1,9 @@
+import { Router, Request, Response } from 'express';
+
+const router = Router();
+
+router.get('/teste', (request: Request, response: Response) => {
+    return response.json({ message: "Hello typescript"});
+})
+
+export { router };
