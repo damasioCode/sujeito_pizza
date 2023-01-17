7184b4985901de572f99338c9f76ab0bc4896da8
Daniel Damasio <damasio.dan7@gmail.com>
2023-01-16 22:35:47 -0300

    added cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
-import express, { Request, Response, NextFunction } from 'express'
+import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 
 import { router } from './routes';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+
 app.use(router);
 
 app.listen(3000, () => {
