ed88d175f48dfcba12a79a577367921430ab9418
Daniel Damasio <damasio.dan7@gmail.com>
2023-01-18 23:00:56 -0300

    feature: create user

--- src/services/user/CreateUserService.ts
+++ src/services/user/CreateUserService.ts
@@ -1,3 +1,6 @@
+import { response } from 'express';
+import prismaClient from '../../prisma'
+
 interface UserRequest {
     name: string;
     email: string;
@@ -6,7 +9,42 @@ interface UserRequest {
 
 class CreateUserService {
     async execute({ name, email, password }: UserRequest) {
-        return { message: name };
+        try {
+            //verifcar se ele enviou um email
+            if(!email){
+                throw 'Email incorrect';
+            }
+
+            const userAlreadyExists = await prismaClient.user.findFirst({
+                where: {
+                    email: email
+                }
+            })
+
+            if(userAlreadyExists){
+                throw 'User already exists';
+            }
+
+            const user = await prismaClient.user.create({
+                data: {
+                    name: name,
+                    email: email,
+                    password: password,
+                },
+                select:{
+                    id: true,
+                    name: true,
+                    email: true,
+                }
+            })
+
+            return user;
+
+        } catch(error) {
+            return { message: error}
+        }
+        
+
     }
 }
 
